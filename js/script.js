e267d45681fd854da1b8ee7863edc281d7355144
Eric Eggert <mail@yatil.net>
2015-02-26 13:04:01 +0100

    Navigation

--- js/script.js
+++ js/script.js
@@ -22,3 +22,31 @@ $('#filters input').on('change', function(e) {
   o = ' <strong>Your selection:</strong> ' + o;
   $('#filtered').html(o);
 });
+
+var $sideBar = $('.navbar-scroll');
+
+$sideBar.affix({
+  offset: {
+    top: function () {
+      var offsetTop      = $sideBar.offset().top
+      var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+      var navOuterHeight = $('.navbar-default').height()
+
+      return (this.top = offsetTop - navOuterHeight - sideBarMargin)
+    },
+    bottom: function () {
+      return 0;
+      // return (this.bottom = $('.bs-docs-footer').outerHeight(true))
+    }
+  }
+});
+
+$sideBar.on('affix.bs.affix', function(e) {
+  $(this).css('width', $(this).parent().width());
+  $(this).css('position', 'fixed');
+});
+
+$sideBar.on('affixed-top.bs.affix', function(e) {
+  $(this).css('width', 'auto');
+  $(this).css('position', 'static');
+});
