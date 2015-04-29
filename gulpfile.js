9e0ad6185b2c2bfadc961cc0c42cc69c1370aa06
Eric Eggert <mail@yatil.net>
2015-04-29 14:35:30 +0200

    Add CSS minification, moved bootstrap and removed
    components

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
+var minifyCss =require('gulp-minify-css');
 
 var sassdir = "_scss/*.scss";
 
@@ -13,6 +14,7 @@ gulp.task('default', function () {
         .pipe(gulp.dest('./css/'))
         .pipe(autoprefixer())
         .pipe(concat('styles.css'))
+        .pipe(minifyCss())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./css/'));
 });
