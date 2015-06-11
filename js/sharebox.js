df6d7eeeba7fd73957c53014c7f66029cfbf6ae1
Eric Eggert <mail@yatil.net>
2015-06-11 16:18:13 +0200

    Add Sharebox

---
+++ js/sharebox.js
@@ -0,0 +1,77 @@
+var addclass = function(el, className) {
+  if (el.classList)
+    el.classList.add(className);
+  else
+    el.className += ' ' + className;
+};
+
+var remclass = function(el, className) {
+  if (el.classList)
+    el.classList.remove(className);
+  else
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
+
+var hasclass = function(el, className) {
+  if (el.classList)
+    return el.classList.contains(className);
+  else
+    return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  var plel = document.createElement('a');
+  //addclass(plel, 'permalink');
+  plel.innerHTML = '<svg class="icon" viewBox="0 0 1024 1024"><path d="M864 704c-45.16 0-85.92 18.738-115.012 48.83l-431.004-215.502c1.314-8.252 2.016-16.706 2.016-25.328s-0.702-17.076-2.016-25.326l431.004-215.502c29.092 30.090 69.852 48.828 115.012 48.828 88.366 0 160-71.634 160-160s-71.634-160-160-160-160 71.634-160 160c0 8.622 0.704 17.076 2.016 25.326l-431.004 215.504c-29.092-30.090-69.852-48.83-115.012-48.83-88.366 0-160 71.636-160 160 0 88.368 71.634 160 160 160 45.16 0 85.92-18.738 115.012-48.828l431.004 215.502c-1.312 8.25-2.016 16.704-2.016 25.326 0 88.368 71.634 160 160 160s160-71.632 160-160c0-88.364-71.634-160-160-160z"></path></svg> SHARE';
+
+  var plwrapdiv = document.createElement('div')
+  addclass(plwrapdiv, 'permalink');
+
+  var sharebox = document.createElement('div');
+  addclass(sharebox, 'sharebox');
+  var shareboxtext = '<p><label>Link to this section:<input type="url" value="%s" readonly> Shortcut to copy the link: <kbd>ctrl</kbd>+<kbd>C</kbd> <em>or</em> <kbd>⌘</kbd><kbd>C</kbd></label></p><p><a href="mailto:?subject=Web%20Accessibility%20Tutorials&body=Hi!%0AYou%20might%20be%20interested%20in%20this%20section%20of%20W3C%20WAI%E2%80%99s%20Web%20Accessibility%20Tutorials%3A%0A%0A%s">E-mail a link to this section</a><button>Close</button></p>';
+
+  var url = window.location.origin + window.location.pathname;
+
+  var elements = document.querySelectorAll('.sc-wrapper h4');
+  Array.prototype.forEach.call(elements, function(el, i){ // … .each(…)
+
+    var cplel = plel.cloneNode(true);
+    cplel.setAttribute('href', '#' + el.id);
+    cplel.setAttribute('aria-label', 'Share Link to the section “' + el.textContent + '”');
+
+    var csbtext = shareboxtext.replace("%s", url + '#' + el.id).replace("%s", url + '#' + el.id);
+    var csb = sharebox.cloneNode(true);
+    csb.innerHTML = csbtext;
+
+    var cplwrapdiv = plwrapdiv.cloneNode(true);
+    cplwrapdiv.appendChild(cplel);
+    cplwrapdiv.appendChild(csb);
+    //addclass(cplwrapdiv, el.localName);
+
+    cplel.addEventListener('click', function(e){
+      var sbox = this.nextSibling;
+      if (hasclass(sbox, 'open')) {
+        remclass(this.nextSibling, 'open');
+      } else {
+        addclass(this.nextSibling, 'open');
+        this.nextSibling.querySelector('input').select();
+      }
+      e.preventDefault();
+
+    });
+
+    el.parentNode.insertBefore(cplwrapdiv, el);
+  });
+
+  var sbbuttons = document.querySelectorAll('.sharebox button');
+  Array.prototype.forEach.call(sbbuttons, function(el, i){ // … .each(…)
+    el.addEventListener('click', function(){
+      var openboxes = document.querySelectorAll('.sharebox.open');
+      for (var i = openboxes.length - 1; i >= 0; i--) {
+        remclass(openboxes[i], 'open');
+      };
+      el.parentNode.parentNode.parentNode.querySelector('a').focus();
+    });
+  });
+});
