71734f476b331fce8876f6adf55249deab55425a
Eric Eggert <mail@yatil.net>
2018-06-01 12:58:30 +0200

    Change font

--- _js/script.js
+++ _js/script.js
@@ -1,17 +1,4 @@
 /*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas, David Knight. Dual MIT/BSD license */
-var supportsWoff2 = (function( win ){
-  if( !( "FontFace" in win ) ) {
-    return false;
-  }
-
-  var f = new FontFace('t', 'url( "data:application/font-woff2;base64,d09GMgABAAAAAAIkAAoAAAAABVwAAAHcAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAABlYAgloKLEoBNgIkAxgLDgAEIAWDcgc1G7IEyB6SJAFID5YA3nAHC6h4+H7s27nP1kTyOoQkGuJWtNGIJKYznRI3VEL7IaHq985ZUuKryZKcAtJsi5eULwUybm9KzajBBhywZ5ZwoJNuwDX5C/xBjvz5DbsoNsvG1NGQiqp0NMLZ7JlnW+5MaM3HwcHheUQeiVokekHkn/FRdefvJaTp2PczN+I1Sc3k9VuX51Tb0Tqqf1deVXGdJsDOhz0/EffMOPOzHNH06pYkDDjs+P8fb/z/8n9Iq8ITzWywkP6PBMMN9L/O7vY2FNoTAkp5PpD6g1nV9WmyQnM5uPpAMHR2fe06jbfvzPriekVTQxC6lpKr43oDtRZfCATl5OVAUKykqwm9o8R/kg37cxa6eZikS7cjK4aIwoyh6jOFplhFrz2b833G3Jii9AjDUiAZ9AxZtxdEYV6imvRF0+0Nej3wu6nPZrTLh81AVcV3kmMVdQj6Qbe9qetzbuDZ7vXOlRrqooFSxCv6SfrDICA6rnHZXQPVcUHJYGcoqa3jVH7ATrjWBNYYkEqF3RFpVIl0q2JvMOJd7/TyjXHw2NyAuJpNaEbz8RTEVtCbSH7JrwQQOqwGl7sTUOtdBZIY2DKqKlvOmPvUxJaURAZZcviTT0SKHCXqzwc=" ) format( "woff2" )', {});
-  f.load()['catch'](function() {});
-
-  return f.status == 'loading' || f.status == 'loaded';
-})( this );
-
-
-/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas, David Knight. Dual MIT/BSD license */
 
 if (!window.matchMedia) {
   window.matchMedia = function() {
@@ -640,14 +627,6 @@ jQuery(document).ready(function($) {
       target: '.overview.spy-active',
       offset: 60
     });
-
-    // Use WOFF2 if supported
-    if( supportsWoff2 ) {
-      loadCSS( location.pathname + "css/vollkorn-woff2.css" );
-    } else {
-      // Default to WOFF
-      loadCSS( location.pathname + "css/vollkorn-woff.css" );
-    }
 
     if(!localStorage.getItem("wai.scrollAnimation")) {
       localStorage.setItem("wai.scrollAnimation", 750);
