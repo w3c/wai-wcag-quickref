eef15ab0e16c02bf9d29f8d371a0f1c53eb3edf2
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2025-03-19 14:03:46 -0400

    Remove individual-SC technique category checkboxes
    These currently don't work on the default branch,
    and were always hidden in the latest published version

--- _js/script.js
+++ _js/script.js
@@ -659,28 +659,6 @@ jQuery(document).ready(function($) {
     svg4everybody();
   }
 
-  $('main').on('change', '.techniques-button input', function(event) {
-    event.preventDefault();
-    var target = $(event.target);
-    var type = target.attr('name').match(/sc-[0-9]{2,3}-(.*)/)[1];
-    if (($('.techniques-button input[name$="' + type + '"]:checked').length > 0) && ($('.techniques-button input[name$="' + type + '"]:not(:checked)').length > 0)) {
-      $('[name="filter-techniques"][value="' + type + '"]').prop('indeterminate', true);
-    } else {
-      $('[name="filter-techniques"][value="' + type + '"]').prop('indeterminate', false);
-      if ($('.techniques-button input[name$="' + type + '"]:checked').length === 0) {
-        $('[name="filter-techniques"][value="' + type + '"]').removeProp('checked');
-      }
-      if ($('.techniques-button input[name$="' + type + '"]:not(:checked)').length === 0) {
-        $('[name="filter-techniques"][value="' + type + '"]').prop('checked', "checked");
-      }
-    }
-    if(target.is(':checked')) {
-      $('#' + target.prop('name')).addClass('active');
-    } else {
-      $('#' + target.prop('name')).removeClass('active');
-    }
-  });
-
   $('main').on('click', '.techniques-button .btn-techniques', function(event) {
     var target = $(event.target),
         cntrls = target.attr('aria-controls'),
